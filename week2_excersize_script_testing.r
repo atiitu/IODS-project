d69ef468def613db17088e5111e1fdb72871fefd
atiitu <44703827+atiitu@users.noreply.github.com>
2018-11-10 21:52:54 +0200

    Read in data

--- week2_excersize_script_testing.r
+++ week2_excersize_script_testing.r
@@ -1,3 +1,9 @@
 # Week 2: Regression model excersize
 # Anna Syreeni
 # This document has all the codes. They are also in chapter2.Rmd file
+
+#First, set working directory to where data is
+setwd("~/Tyojuttuja/opinnot/GitHub/IODS-project/data")
+
+#Read in data
+
