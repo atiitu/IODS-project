09198b9ba2bb333c831b12876354568f1e891a08
atiitu <44703827+atiitu@users.noreply.github.com>
2018-11-10 22:38:21 +0200

    Summaries added

--- week2_excersize_script_testing.r
+++ week2_excersize_script_testing.r
@@ -6,4 +6,33 @@
 setwd("~/Tyojuttuja/opinnot/GitHub/IODS-project/data")
 
 #Read in data
+data <- read.table(file = "learning2014.txt", sep="\t")
+
+#Look that variables are OK
+str(data) #looks good!
+head(data)
+
+#Summaries of all variables in the data are found below:
+
+summary(data$gender)
+summary(data$age)
+summary(data$attitude)
+summary(data$deep)
+summary(data$stra)
+summary(data$surf)
+summary(data$points)
+
+Show a graphical overview of the data 
+and show summaries of the variables in the data.
+Describe and interpret the outputs,
+commenting on the distributions of the variables 
+and the relationships between them. (0-3 points)
+
+#Summary of all data
+# access the GGally and ggplot2 libraries
+library(GGally)
+library(ggplot2)
+
+
+
 
